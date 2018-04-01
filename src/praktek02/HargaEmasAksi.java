abee3cf71d3e1f766cd14b7c296c715648bb956c
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-04-01 11:26:07 +0700

    Instance Object

--- src/praktek02/HargaEmasAksi.java
+++ src/praktek02/HargaEmasAksi.java
@@ -1,6 +1,8 @@
 package praktek02;
 public class HargaEmasAksi {
     public static void main(String[] args) {
-        
+        HargaEmas ana = new HargaEmas();
+        HargaEmas ani = new HargaEmas();
+        HargaEmas anu = new HargaEmas();
     }
 }
