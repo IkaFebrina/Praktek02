685960e826dd508c3989194fbca64007d22ac597
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-04-01 11:17:41 +0700

    Membuat Attribute dan Behavior

--- src/praktek02/HargaEmas.java
+++ src/praktek02/HargaEmas.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class HargaEmas {
+    double Berat_Emas;
+    double Harga_Per_Gram;
     
+    void cetakInfo(){
+        System.out.println("==================================");
+        System.out.println("Berat_Emas      :"+Berat_Emas);
+        System.out.println("Harga_Per_Gram  :"+Harga_Per_Gram);
+        System.out.println("==================================");
+    }
 }
